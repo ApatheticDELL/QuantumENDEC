f4bb3bb72778899114273b903596eefd8640ef90
Dell <92345385+ApatheticDELL@users.noreply.github.com>
2023-11-23 13:53:34 -0500

    Update check.py
    Updated to check filters for <msgType>

--- check.py
+++ check.py
@@ -10,6 +10,7 @@ def ConfigFilters(InputXML, ConfigDt):
         Final = ConfigDt[re.search(r'<status>\s*(.*?)\s*</status>', InputXML, re.MULTILINE | re.IGNORECASE | re.DOTALL).group(1)]
         Final = ConfigDt[re.search(r'<severity>\s*(.*?)\s*</severity>', InputXML, re.MULTILINE | re.IGNORECASE | re.DOTALL).group(1)]
         Final = ConfigDt[re.search(r'<urgency>\s*(.*?)\s*</urgency>', InputXML, re.MULTILINE | re.IGNORECASE | re.DOTALL).group(1)]
+        Final = ConfigDt[re.search(r'<msgType>\s*(.*?)\s*</msgType>', InputXML, re.MULTILINE | re.IGNORECASE | re.DOTALL).group(1)] #new
         
     if len(ConfigDt['AllowedLocations_Geocodes']) == 0:
         pass
