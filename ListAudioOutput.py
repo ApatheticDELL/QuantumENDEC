76eec4ea4539a6d8b27bd30a39a021e1daf9b06c
Dell <92345385+ApatheticDELL@users.noreply.github.com>
2024-01-11 18:50:49 -0500

    Delete ListAudioOutput.py

--- ListAudioOutput.py
+++
@@ -1,7 +0,0 @@
-import sounddevice as sd
-
-print(sd.query_devices())
-print("")
-print("This is a list of audio output devices alert audio is able to output to! To select a device, copy its name into the SpecifiedAudioDevice feild in the config.json")
-
-exit()
